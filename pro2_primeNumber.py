0ad4392c931dea690cf3b808d5267b17c666812e
abhi07cemk <abhinandanbanerjeekgp@gmail.com>
2026-02-25 20:44:05 +0530

    Made changes

--- pro2_primeNumber.py
+++ pro2_primeNumber.py
@@ -1,12 +1,12 @@
-def generate_primes(n):
-    for num in range(2, n + 1):
-        is_prime = True
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                is_prime = False
-                break
-        if is_prime:
-            print(num, end=" ")
-
 n = int(input("Enter n: "))
-generate_primes(n)
+
+for i in range(2, n + 1):
+    is_prime = True
+
+    for j in range(2, int(i ** 0.5) + 1):
+        if i % j == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(i, end=" ")
